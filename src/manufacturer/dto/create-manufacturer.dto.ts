152e29f1e694413457ec35814cae0ac9ed93c931
iyustlop <iyustlop@gmail.com>
2024-06-25 17:36:38 +0200

    Added create-manufacturer-dto

--- src/manufacturer/dto/create-manufacturer.dto.ts
+++ src/manufacturer/dto/create-manufacturer.dto.ts
@@ -1,5 +1,10 @@
+import { IsNumber, IsString } from "class-validator";
+
 export class CreateManufacturerDto {
+    @IsNumber()
     id: number;
+    @IsString()
     manufacturer: string;
+    @IsString()
     location: string;
 }
