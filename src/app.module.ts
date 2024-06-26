792ad147de38da06940276c260b135a9881036b4
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-06-27 00:33:58 +0200

    users initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ManufacturerModule } from './manufacturer/manufacturer.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ManufacturerModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ManufacturerModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+  ],
   providers: [],
 })
 export class AppModule {}
