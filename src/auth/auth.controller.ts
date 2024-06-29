36926e7619c6b3dedd9a1572aad289f7873c0eca
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-06-29 22:52:43 +0200

    improve with some swagger options

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { LoginDto } from './dto/login.dto';
 import { Request } from 'express';
 import { Role } from './enums/role.enum';
 import { Auth } from './decorators/auth.decorator';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 interface RequestWithUser extends Request {
   user: {
@@ -14,22 +15,35 @@ interface RequestWithUser extends Request {
 }
 
 @Controller('auth')
+@ApiTags('Authentication')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new User' })
+  @ApiResponse({ status: 200, description: 'Create a new User' })
+  @ApiResponse({ status: 401, description: 'Unathorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
   register(@Body() createRegisterDto: CreateRegisterDto) {
     console.log(createRegisterDto);
     return this.authService.register(createRegisterDto);
   }
 
   @Post('login')
+  @ApiOperation({ summary: 'Login with a User, returns JWT' })
+  @ApiResponse({ status: 200, description: 'Create a new User' })
+  @ApiResponse({ status: 401, description: 'Unathorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
   @Get('profile')
   @Auth(Role.ADMIN)
+  @ApiOperation({ summary: 'Show User Profile information with ADMIN Role' })
+  @ApiResponse({ status: 200, description: 'Create a new User' })
+  @ApiResponse({ status: 401, description: 'Unathorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
   profile(@Req() req: RequestWithUser) {
     return this.authService.profile(req.user);
   }
